74205480a71708f2af975c6b421f8f3994c3c66d
yuanjiajun <768747811@qq.com>
2020-02-07 14:52:08 +0800

    20200207

---
+++ test/hashmap/MapMergeTest.java
@@ -0,0 +1,14 @@
+package hashmap;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapMergeTest {
+
+    @Test
+    public void testMapMerge() {
+        MapMerge mapMerge = new MapMerge();
+        mapMerge.key = "tmp";
+    }
+}
