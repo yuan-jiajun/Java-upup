88befef1423dc94d3254c0ba0342e26716935dcf
yuanjiajun <767847811@qq.com>
2020-03-18 02:12:39 +0800

    FileContentReplace not well v0.1

--- java/src/test/Split.java
+++ java/src/test/Split.java
@@ -6,27 +6,8 @@ package test;
  * @date 2019/11/24 17:09
  */
 public class Split {
-    private String string =new String("yuan jia jun ");
-    private String[] locate_line_split = string.split(" ");
-
     public static void main(String[] args) {
-        Split split = new Split();
-        System.out.println(split.getLocate_line_split().length);
-    }
-
-    public String getString() {
-        return string;
-    }
-
-    public void setString(String string) {
-        this.string = string;
-    }
-
-    public String[] getLocate_line_split() {
-        return locate_line_split;
-    }
-
-    public void setLocate_line_split(String[] locate_line_split) {
-        this.locate_line_split = locate_line_split;
+        String string = "yuanjiajun";
+        System.out.println(string.split("yuan")[1]);
     }
 }
