2c91b15bacdc31c2a26eea3e18ce9b45d999bbe1
yuanjiajun <768747811@qq.com>
2021-01-23 17:54:37 +0800

    加入scala编程

---
+++ src/main/java/spark-scala/src/JavaHello.java
@@ -0,0 +1,10 @@
+/**
+ * @author yuanjiajun
+ * @description
+ * @date 2021/1/23 17:50
+ */
+public class JavaHello {
+    public static void main(String[] args) {
+        System.out.println("Java hello");
+    }
+}
---
+++ src/main/java/spark-scala/src/ScalaHello.scala
@@ -0,0 +1,5 @@
+object ScalaHello {
+  def main(args: Array[String]): Unit = {
+    println("Scala hello")
+  }
+}
